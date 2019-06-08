e5eab9fc71a27003f000854b9495658ccd4045f2
Victor Dremov (Виктор Дремов) <37803199+dremovv@users.noreply.github.com>
2019-06-08 17:54:41 +0300

    add pythonanywhere to allowed hosts

--- ab_testing_strategies/settings.py
+++ ab_testing_strategies/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'k6w5kz)^7l3xi+gv^nvf#(-rsqo0x(li5b7#^vle)+n8hqb^!k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['victordremov.pythonanywhere.com']
 
 # Application definition
 
